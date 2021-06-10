e6ff8e0dfe3300c00ebc9628d54f0f67d763e177
Ashley Burns <Ash_burn999@yahoo.com>
2021-06-10 19:13:30 -0400

    restart

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,23 @@
-import { Route, Switch } from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-
-import "@material-tailwind/react/tailwind.css"
-import Navigation from './components/HomePage/Nav'
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-
-
-      {/* <Switch>
-        <Route exact path='/'>
-      
-        </Route>
-      </Switch> */}
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
